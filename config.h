c61bae6ecaf7a490eadeb3122a1b9d7298c7c3bd
Nicholas Tay <nick@windblume.net>
2022-01-06 00:00:29 +1100

    Reindent config.h

--- config.h
+++ config.h
@@ -1,10 +1,12 @@
 #define DEFAULT_GRAMMAR "Cvccvc!##"
 
-// For vowels:
-//   i, o excluded due to potentially confusing 1/l/i + 0/o
-//   y included as a vowel because it kinda is one
+/*
+ * For vowels:
+ *   i, o excluded due to potentially confusing 1/l/i + 0/o
+ *   y included as a vowel because it kinda is one
+ */
 #define CLASSES \
-  CLASS('v', "aeuy") \
-  CLASS('c', "bcdfghkmnprstvwxz") \
-  CLASS('#', "1234567890") \
-  CLASS('!', "@#$%^&*_-+=()[]{}")
+    CLASS('v', "aeuy") \
+    CLASS('c', "bcdfghkmnprstvwxz") \
+    CLASS('#', "1234567890") \
+    CLASS('!', "@#$%^&*_-+=()[]{}")
